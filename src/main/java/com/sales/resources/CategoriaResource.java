03613a781d1365f40071a1f31f6d33d8d0d2777e
will henrique <willhc1984@gmail.com>
2023-02-14 09:18:29 -0300

    Categorias - teste com postman

--- src/main/java/com/sales/resources/CategoriaResource.java
+++ src/main/java/com/sales/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.sales.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sales.domain.Categoria;
+
 @RestController
-@RequestMapping(value = "/categoria")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String index() {
-		return "Hello World";
+	public List<Categoria> index() {
+		Categoria cat1 = new Categoria(1L, "Informática");
+		Categoria cat2 = new Categoria(2L, "Almoxarifado");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat2);
+		categorias.add(cat1);
+		
+		return categorias;
 	}
 
 }
