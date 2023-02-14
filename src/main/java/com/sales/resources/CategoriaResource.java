660141e050e45984b105123b1e35f843e4ecfd21
will henrique <willhc1984@gmail.com>
2023-02-14 10:54:50 -0300

    Categoria Service/Controller

--- src/main/java/com/sales/resources/CategoriaResource.java
+++ src/main/java/com/sales/resources/CategoriaResource.java
@@ -2,27 +2,40 @@ package com.sales.resources;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sales.domain.Categoria;
+import com.sales.service.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	@GetMapping
-	public List<Categoria> index() {
-		Categoria cat1 = new Categoria(1L, "Informática");
-		Categoria cat2 = new Categoria(2L, "Almoxarifado");
-		
-		List<Categoria> categorias = new ArrayList<>();
-		categorias.add(cat2);
-		categorias.add(cat1);
-		
-		return categorias;
+	public ResponseEntity<List<Categoria>> buscarTodos() {
+		return ResponseEntity.status(HttpStatus.OK).body(categoriaService.buscarTodos());
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Object> buscarPorId(@PathVariable Long id){
+		Optional<Categoria> categoriaOptional = categoriaService.buscarPorId(id);
+		if(!categoriaOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Categoria não encontrada!");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(categoriaOptional.get());
 	}
 
 }
---
+++ src/main/java/com/sales/service/CategoriaService.java
@@ -0,0 +1,27 @@
+package com.sales.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sales.domain.Categoria;
+import com.sales.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired 
+	private CategoriaRepository categoriaRepository;
+	
+	public Optional<Categoria> buscarPorId(Long id) {
+		Optional<Categoria> categoriaOptional = categoriaRepository.findById(id);
+		return categoriaOptional;
+	}
+	
+	public List<Categoria> buscarTodos() {
+		return categoriaRepository.findAll();
+	}
+
+}
